f10752686ec6bf11a74f6d96de9a4af5b06a4989
terpevt <95994267+terpevt@users.noreply.github.com>
2021-12-12 09:41:51 +0400

    Add files via upload

---
+++ Kata Academy/src/com/company/terpevt/Roma.java
@@ -0,0 +1,25 @@
+package com.company.terpevt;
+
+public enum Roma {
+    ROMA,
+    I,
+    II,
+    III,
+    IV,
+    V,
+    VI,
+    VII,
+    VIII,
+    IX,
+    X,
+    XI,
+    XII,
+    XIII,
+    XIV,
+    XV,
+    XVI,
+    XVII,
+    XVIII,
+    XIX,
+    XX
+}
